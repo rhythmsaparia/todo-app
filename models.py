00ff82d95e5e56075e65d0b2c8e993f193b03f3f
Rhythm Saparia <rhythmsaparia@gmail.com>
2024-08-30 22:51:41 +0530

    feat: added authentication

--- models.py
+++ models.py
@@ -17,6 +17,7 @@ class Users(Base):
 
 class Todos(Base):
     __tablename__ = 'todos'
+
     id = Column(Integer, primary_key=True, index=True) 
     title = Column(String)
     description = Column(String)
